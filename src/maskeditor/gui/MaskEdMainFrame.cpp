c1d226e3cd3320670df5f51cbfcf525bad56206a
fmannan <fmannan@users.sourceforge.net>
2008-08-18 20:26:43 +0000

    +Updated mask saving option in Mask Editor to store all the masks as tif files. The user needs to provide the prefix and mask would be generated as <prefix>_<image no.>.tif Later on, I'll add option for specifying the output format.

--- src/maskeditor/gui/MaskEdMainFrame.cpp
+++ src/maskeditor/gui/MaskEdMainFrame.cpp
@@ -171,11 +171,12 @@ void MaskEdMainFrame::OnSaveImage(wxCommandEvent &event)
 void MaskEdMainFrame::OnSaveMask(wxCommandEvent &event)
 {
     //wxString filter = wxT("JPEG files (*.jpg;*.jpeg)|*.jpg;*.JPG;*.jpeg;*.JPEG|tiff files (*.tif)|*.tif|All files (*.*)|*.*");
-    wxFileDialog dialog(this, wxT("Save Mask"), wxEmptyString,
+    wxFileDialog dialog(this, wxT("Save Mask: Enter prefix"), wxEmptyString,
         wxEmptyString, wxEmptyString, wxSAVE);
     if(dialog.ShowModal() == wxID_OK)
     {
-        m_MaskEdClientWnd->saveMask(0, dialog.GetPath());
+        //m_MaskEdClientWnd->saveMask(0, dialog.GetPath());
+        MaskMgr::getInstance()->saveMask(string(dialog.GetPath().mb_str()));
     }
 
 }
