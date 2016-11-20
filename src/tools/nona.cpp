74aa604d72568cb6081481ec39cc6e4dad26923e
tmodes <tmodes@users.sourceforge.net>
2016-11-20 17:18:56 +0100

    Nona: Recognize also --help switch

--- src/tools/nona.cpp
+++ src/tools/nona.cpp
@@ -148,6 +148,7 @@ int main(int argc, char* argv[])
         { "create-exposure-layers", no_argument, NULL, EXPOSURELAYERS },
         { "clip-exposure", optional_argument, NULL, MASKCLIPEXPOSURE },
         { "seam", required_argument, NULL, SEAMMODE},
+        { "help", no_argument, NULL, 'h'},
         0
     };
     
