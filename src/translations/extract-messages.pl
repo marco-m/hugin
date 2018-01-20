5ca9b7da8479701235d3520c50ffe0f7a930beb7
tmodes <tmodes@users.sourceforge.net>
2018-01-20 18:55:14 +0100

    Some improvements to extract-messages.pl script (Bruno Postle)

--- src/translations/extract-messages.pl
+++ src/translations/extract-messages.pl
@@ -23,18 +23,26 @@ if(!defined $msgmerge) { die 'ERROR: msgmerge not found'; }
 my $basedir=getcwd;
 say 'Found all necessary programs';
 
+my $path_xrc_cpp=File::Spec->rel2abs('xrc.cpp', $basedir);
+my $path_infiles_list=File::Spec->rel2abs('infiles.list', $basedir);
+my $path_potfiles_in=File::Spec->rel2abs('POTFILES.in', $basedir);
+my $path_project_pot=File::Spec->rel2abs("$project.pot", $basedir);
+
 # parse all xrc files, create xrc.cpp for xgettext input
-unlink 'xrc.cpp';
-unlink 'infiles.list';
+unlink $path_xrc_cpp;
+unlink $path_infiles_list;
 chdir "..";
+open(my $xrc_cpp, '>', $path_xrc_cpp);
 find({wanted => sub { if (-f and /\.xrc$/)
   {
     my $filename=File::Spec->abs2rel($File::Find::name);
     if ($^O eq 'MSWin32') {$filename =~ tr#\\#/#;};
     say("Processing $filename");
-    system("$wxrc -g $filename >>$basedir/xrc.cpp");
+    my @s=qx/$wxrc -g $filename/;
+    say $xrc_cpp @s;
   };
 }, no_chdir => 1}, getcwd);
+close $xrc_cpp;
 
 #create array of all C++ files
 my @cfiles;
@@ -48,28 +56,30 @@ find({wanted => sub {  if (-f) {
 # create input file list for xgettext
 say 'Extracting messages';
 # first write all C++ files in file
-open(my $filelist, '>', "$basedir/infiles.list") or die 'ERROR: Could not open file $basedir/infiles.list';
+open(my $filelist, '>', $path_infiles_list) or die "ERROR: Could not open file $path_infiles_list";
 foreach (@cfiles) {say $filelist $_;};
 # then add files from POTFILES.in (e.g. tips)
-open(my $potfilein, '<', "$basedir/POTFILES.in") or die 'ERROR: Could not open file $basedir/POTFILES.in';
+open(my $potfilein, '<', $path_potfiles_in) or die "ERROR: Could not open file $path_potfiles_in";
 while(<$potfilein>){say $filelist $_;};
 close $potfilein;
 close $filelist;
 
 # run xgettext
 chdir $basedir;
-system("$xgettext --from-code=UTF-8 --c++ --keyword=_ --copyright-holder=\"$copyright\" --msgid-bugs-address=\"$bugaddr\" --files-from=infiles.list --directory=\"$basedir\" --directory=\"$cstartdir\" --output=$project.pot")==0 or die 'ERROR during running xgettext';
+system($xgettext, '--from-code=UTF-8', '--c++', '--keyword=_', '--copyright-holder='.$copyright,
+    '--msgid-bugs-address='.$bugaddr, '--files-from='.$path_infiles_list, '--directory='.$basedir,
+    '--directory='.$cstartdir, '--output='.$path_project_pot)==0 or die "ERROR during running $xgettext";
 say 'Done extracting messages';
 
 # update all po files with new template
 say 'Merging translations';
 find(sub{if (-f and /\.po$/) {
-    system("msgmerge --verbose -o $_.new.po $_ $basedir/$project.pot")==0 or die 'ERROR during running msgmerge';
+    system($msgmerge, '--verbose', '-o', "$_.new.po", $_, $path_project_pot)==0 or die "ERROR during running $msgmerge";
     rename("$_.new.po", $_);
   }}, '.');
 say 'Done merging translations';
-unlink 'xrc.cpp';
-unlink 'infiles.list';
+unlink $path_xrc_cpp;
+unlink $path_infiles_list;
 # unify line breaks to unix
 say 'Unify line breaks';
 system("perl unix_linebreaks.pl $project.pot *.po outdated/*.po");
