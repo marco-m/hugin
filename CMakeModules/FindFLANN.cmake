f2b96e883c7d077022df99fde1c8c56dd3de67b7
tmodes <tmodes@users.sourceforge.net>
2018-11-01 20:30:06 +0100

    Update flann search module (initial patch by Debian photo team)

--- CMakeModules/FindFLANN.cmake
+++ CMakeModules/FindFLANN.cmake
@@ -8,18 +8,27 @@
 # FLANN_LIBRARIES
 # FLANN_FOUND
 
+if(UNIX)
+  find_package(PkgConfig QUIET)
+  pkg_check_modules(FLANN flann)
+else()
+  find_path(FLANN_INCLUDE_DIRS flann/flann.hpp)
 
-find_path(FLANN_INCLUDE_DIRS flann/flann.hpp)
+  if(FLANN_INCLUDE_DIRS)
+    find_library(FLANN_LIBRARIES flann_cpp)
+    if(FLANN_LIBRARIES)
+      set(FLANN_FOUND "YES")
+    endif()
+  else()
+    set(FLANN_FOUND "NO")
+  endif()
+endif()
 
-if(FLANN_INCLUDE_DIRS)
-  find_library(FLANN_LIBRARIES flann_cpp)
-  if(FLANN_LIBRARIES)
+if(FLANN_FOUND)
     MESSAGE(STATUS "flann library found")
-    set(FLANN_FOUND "YES")
-  endif(FLANN_LIBRARIES)
-else(FLANN_INCLUDE_DIRS)
+else()
   message(STATUS "flann library not found. falling back to included copy")
   set(FLANN_LIBRARIES "flann_cpp")
   set(FLANN_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/src/foreign")
-endif(FLANN_INCLUDE_DIRS)
+endif()
 
